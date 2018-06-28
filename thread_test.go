f6b3ccccdb95f684d990398acf6e87caba875239
Richard Stöckl <richard.stoeckl@aon.at>
2018-06-28 17:33:11 +0200

    added benchmark for threding

--- thread_test.go
+++ thread_test.go
@@ -7,15 +7,34 @@ import (
 	"time"
 )
 
-func BenchmarkThreading(b *testing.B) {
+func BenchmarkStaticThreading(b *testing.B) {
+	events := massEvents()
+	conf = &Configuration{Threads: 4, RestHost: "127.0.0.1:8071", Host: "127.0.0.1", Port: 8071}
+	b.ResetTimer()
+	writeEvents(events, dummyWriter{}, musicianConvert, staticThreads)
+}
+
+func BenchmarkDynamicThreading(b *testing.B) {
+	events := massEvents()
+	conf = &Configuration{Threads: 4, RestHost: "127.0.0.1:8071", Host: "127.0.0.1", Port: 8071}
+	b.ResetTimer()
+	writeEvents(events, dummyWriter{}, musicianConvert, dynamicThreads)
+}
+
+func BenchmarkMainThreading(b *testing.B) {
+	events := massEvents()
+	conf = &Configuration{Threads: 4, RestHost: "127.0.0.1:8071", Host: "127.0.0.1", Port: 8071}
+	b.ResetTimer()
+	writeEvents(events, dummyWriter{}, musicianConvert, mainThread)
+}
+
+func massEvents() *[]*model.Event {
 	e := &model.Event{Name: "Frühschoppen", Date: time.Date(2018, 6, 17, 9, 0, 0, 0, time.Local)}
 	events := make([]*model.Event, 1000000)
 	for i := range events {
 		events[i] = e
 	}
-	conf = &Configuration{Threads: 4, RestHost: "127.0.0.1:8071", Host: "127.0.0.1", Port: 8071}
-	b.ResetTimer()
-	writeEvents(&events, dummyWriter{})
+	return &events
 }
 
 type dummyWriter struct {
